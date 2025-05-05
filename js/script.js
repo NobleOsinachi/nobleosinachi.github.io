e8953339587fcfb5c3197d12ec9db879e1663bfb
Noble Osinachi <nobleosinachi98@gmail.com>
2025-05-05 18:21:32 +0100

    Update the home page form API

--- js/script.js
+++ js/script.js
@@ -38,21 +38,64 @@ $(document).ready(function () {
     });
 
     // <!-- emailjs to mail contact form data -->
-    $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
-
+ /*
+ $("#contact-form").submit(function (event) {
+     emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+     
         emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById("contact-form").reset();
+            alert("Form Submitted Successfully");
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert("Form Submission Failed! Try Again");
+        });
         event.preventDefault();
     });
+    */
     // <!-- emailjs to mail contact form data -->
+
+
+
+
+
+
+ document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+const url = "https://nobleosinachi-email-api.onrender.com/homepage-form"
+    fetch(url, {
+      method: "POST",
+      body: new URLSearchParams(formData),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        alert(data.message);
+        form.reset();
+      } else {
+        alert("Error: " + data.message);
+      }
+    })
+    .catch(err => {
+      console.error("Submission error:", err);
+      alert("Something went wrong.");
+    });
+  });
+
+
+
+
+
+
+
 
 });
 
